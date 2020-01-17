5d71d2eef0f2e4bfdbd715a2c9dd86ed79118989
dweinmann <dontspamme@gmx.org>
2020-01-17 22:49:25 +0100

    Add GamefieldSolver (WIP!)
    Change-Id: I509b0be42016e9800cd48daed65d2599f2afdff0

--- SchiffeVersenken/Game.cs
+++ SchiffeVersenken/Game.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using static SchiffeVersenken.Helper;
 
 namespace SchiffeVersenken
 {
@@ -40,16 +42,23 @@ namespace SchiffeVersenken
             {
                 gameField = new GameFieldConsole(fieldSize);
             }
+            
+            DoSimulationGame(gameField);
+            //DoHumanGame(gameField);
 
+        }
+
+        private void DoHumanGame(GameField gameField)
+        {
             do
             {
                 gameField.UpdateField();
                 Console.WriteLine(@"'x' oder 'q' zum Beenden");
 
                 int x = Helper.GetIntInput("Bitte X-Koordinate eingeben: ", 0, fieldSize - 1);
-                Helper.CheckExitValue(x);
+                Helper.ExitOnExitValue(x);
                 int y = Helper.GetIntInput("Bitte Y-Koordinate eingeben: ", 0, fieldSize - 1);
-                Helper.CheckExitValue(y);
+                Helper.ExitOnExitValue(y);
 
                 gameField.Shoot(x, y);
 
@@ -57,5 +66,150 @@ namespace SchiffeVersenken
 
             gameField.PlayerWonMessage();
         }
+
+        private void DoSimulationGame(GameField gameField)
+        {
+            int[,] shootField = new int[fieldSize, fieldSize];
+            Point lastHitPoint = new Point();
+            Direction lastDirection = Direction.None;
+            Direction priorToLastDirection = Direction.None;
+            Random rnd = new Random();
+
+            do
+            {
+                gameField.UpdateField();
+                int x = 0, y = 0;
+                do
+                {
+                    if(lastDirection == Direction.None)
+                    {
+                        x = rnd.Next(0, fieldSize);
+                        y = rnd.Next(0, fieldSize);
+                    }
+                    else
+                    {
+                        switch (lastDirection)
+                        {
+                            case Direction.Up:
+                                x = lastHitPoint.X;
+                                y = lastHitPoint.Y - 1;
+                                if (IsOutsideOfField(x, y))
+                                {
+                                    lastDirection = Direction.Down;
+                                    y = lastHitPoint.Y+1;
+                                    if (IsAlreadyShot(shootField, x, y))
+                                    {
+                                        if(rnd.Next()%2 == 0)
+                                        {
+                                            lastDirection = Direction.Left;
+                                        }
+                                        else
+                                        {
+                                            lastDirection = Direction.Right;
+                                        }
+                                    }
+                                }
+                                break;
+                            case Direction.Down:
+                                x = lastHitPoint.X;
+                                y = lastHitPoint.Y + 1;
+                                if (IsOutsideOfField(x, y))
+                                {
+                                    lastDirection = Direction.Up;
+                                    y = lastHitPoint.Y-1;
+                                }
+                                break;
+                            case Direction.Left:
+                                x = lastHitPoint.X - 1;
+                                y = lastHitPoint.Y;
+                                if (IsOutsideOfField(x, y))
+                                {
+                                    lastDirection = Direction.Right;
+                                    x = lastHitPoint.X+1;
+                                }
+                                break;
+                            case Direction.Right:
+                                x = lastHitPoint.X + 1;
+                                y = lastHitPoint.Y;
+                                if (IsOutsideOfField(x, y))
+                                {
+                                    lastDirection = Direction.Left;
+                                    x = lastHitPoint.X-1;
+                                }
+                                break;
+                        }
+                        //if(IsOutsideOfField(x, y))
+                        //{
+                        //    if(priorToLastDirection != Direction.None)
+                        //    {
+                        //        switch (lastDirection)
+                        //        {
+                        //            case Direction.Up:
+                        //                lastHitPoint.Y++;
+                        //                lastDirection = Direction.Down;
+                        //                break;
+                        //            case Direction.Down:
+                        //                lastHitPoint.Y--;
+                        //                lastDirection = Direction.Up;
+                        //                break;
+                        //            case Direction.Left:
+                        //                lastHitPoint.X++;
+                        //                lastDirection = Direction.Right;
+                        //                break;
+                        //            case Direction.Right:
+                        //                lastHitPoint.X--;
+                        //                lastDirection = Direction.Left;
+                        //                break;
+                        //        }
+                        //        priorToLastDirection = Direction.None;
+                        //    } 
+                        //    else
+                        //    {
+                        //    }
+                        //    continue;
+                        //}
+                    } 
+
+                    if(!IsAlreadyShot(shootField, x, y))
+                    {
+                        shootField[x, y] = 1;
+                        break;
+                    }
+                } while (true);
+
+                ShootResult sRes = gameField.Shoot(x, y);
+                if (sRes == ShootResult.Hit)
+                {
+                    priorToLastDirection = lastDirection;
+
+                    if(lastDirection == Direction.None)
+                    {
+                        lastDirection = (Direction)rnd.Next(1, 5);
+                    }
+
+                    lastHitPoint = new Point { X = x, Y = y };
+                } 
+                else if(sRes == ShootResult.DestroyShip)
+                {
+                    lastDirection = Direction.None;
+                }
+
+                Thread.Sleep(10);
+            } while (!gameField.PlayerHasWon());
+
+            gameField.PlayerWonMessage();
+
+        }
+
+        private bool IsOutsideOfField(int x, int y)
+        {
+            return x < 0 || x >= fieldSize || y < 0 || y >= fieldSize;
+        }
+
+        private bool IsAlreadyShot(int[,] shootField, int x, int y)
+        {
+            return shootField[x, y] == 1;
+        }
+ 
     }
 }
---
+++ SchiffeVersenken/Shooter/Shooter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SchiffeVersenken.Helper;
+
+namespace SchiffeVersenken.Shooter
+{
+    public class Shooter
+    {
+    //    GameField gameField;
+    //    int[,] shootField;
+
+
+
+
+    //    public Shooter(GameField gameField)
+    //    {
+    //        this.gameField = gameField;
+    //        shootField = new int[gameField.FieldSize, gameField.FieldSize];
+
+
+    //    }
+
+    //}
+
+    //public abstract class Shoot
+    //{
+    //    Point lastHitPoint = new Point();
+    //    Direction lastDirection = Direction.None;
+    //    Direction priorToLastDirection = Direction.None;
+    //    protected Random rnd = new Random();
+
+    //    public abstract Shoot DoShot(GameField gameField);
+    //}
+
+    //public class GoRandom : Shoot
+    //{
+    //    public override Shoot DoShot(GameField gameField)
+    //    {
+    //        int x = rnd.Next(0, gameField.FieldSize);
+    //        int y = rnd.Next(0, gameField.FieldSize);
+
+
+    //    }
+    //}
+
+    //public class GoRight
+    //{
+
+    }
+}
