b5a1b4c548514cf44b445a94cd8413add9dab2a7
dweinmann <dennis.weinmann@carneios.de>
2020-01-20 07:38:24 +0100

    Add gameloop for multiple rounds
    Change-Id: I8926ce7f326ba8b839665a38b4ac3f3f8c7e94be

--- SchiffeVersenken/Program.cs
+++ SchiffeVersenken/Program.cs
@@ -11,13 +11,17 @@ namespace SchiffeVersenken
         static void Main()
         {
             //Console.OutputEncoding = System.Text.Encoding.UTF8;
-            int fieldSize = Helper.GetIntInput("Bitte Feldgröße angeben ({min}-{max}) Standard ist {defaultValue}: ", 10, 20, 10);
-            Helper.ExitOnExitValue(fieldSize);
 
-            int nrOfRounds = Helper.GetIntInput("Spielen (0) oder automatisch lösen lassen (1-1000 Runden)? ", 0, 1000, 0);
+            do
+            {
+                int fieldSize = Helper.GetIntInput("Bitte Feldgröße angeben ({min}-{max}) Standard ist {defaultValue}: ", 10, 20, 10);
+                Helper.ExitOnExitValue(fieldSize);
 
-            Game schiffeVersenken = new Game(fieldSize, nrOfRounds);
-            schiffeVersenken.Run();
+                int nrOfRounds = Helper.GetIntInput("Spielen (0) oder automatisch lösen lassen (1-1000 Runden)? ", 0, 1000, 0);
+
+                Game schiffeVersenken = new Game(fieldSize, nrOfRounds);
+                schiffeVersenken.Run();
+            } while (true);
         }
     }
 }
